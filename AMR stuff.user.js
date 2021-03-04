5943111fcf039ae08606fe0af7828f0529c618ed
robo <pokemon097@hotmail.co.uk>
2021-03-04 03:31:17 +0000

    some fixes

--- AMR stuff.user.js
+++ AMR stuff.user.js
@@ -98,79 +98,92 @@ GM_registerMenuCommand("Config for AMR Manga Reading", function () {
 /* #endregion */
 
 let observerr = new MutationObserver(callbackk);
-
+let start = 0
 function callbackk(mutations) {
     if (document.querySelector("#amrapp div.v-select__selection.v-select__selection--comma")) {
-        observerr.disconnect()
-
-        //no boarders in continuous scroll mode
-        if (GM_config.get('webtoon')) {
-            document.querySelector("#amrapp > div.v-application--wrap > main > div > div > table").classList.add('webtoon');
-        }
-
-        //code probably only i need
-        if (GM_config.get('mangaTitle')) {
-            if (document.querySelector("head > title").textContent != 'Manga') {
-                document.querySelector("head > title").textContent = 'Manga'
-            }
-        } else if (GM_config.get('mangaTitleName')) {
-            if (!(document.querySelector("h4 > a") == null || document.querySelector("div.v-select__selection.v-select__selection--comma") == null)) {
-                if (document.querySelector("head > title").textContent != document.querySelector("h4 > a").textContent + ' ' + document.querySelector("div.v-select__selection.v-select__selection--comma").textContent) {
-                    document.querySelector("head > title").textContent = document.querySelector("h4 > a").textContent + ' ' + document.querySelector("div.v-select__selection.v-select__selection--comma").textContent
+        if (mutations[0].target == document.querySelector('div.v-select__selections')) {
+            if (GM_config.get('halfFix')) {
+                //scroll to top needs jquery
+                if (typeof jQuery == 'undefined') {
+                    var headTag = document.getElementsByTagName("head")[0];
+                    var jqTag = document.createElement('script');
+                    jqTag.type = 'text/javascript';
+                    jqTag.src = 'https://code.jquery.com/jquery-3.5.1.js';
+                    jqTag.onload = myJQueryCode;
+                    headTag.topAnim(jqTag);
+                } else {
+                    topAnim();
                 }
             }
         }
 
-        //if no jquiry then add jquiry
-        if (GM_config.get('magicScroll')) {
-            if (typeof jQuery == 'undefined') {
-                var headTag = document.getElementsByTagName("head")[0];
-                var jqTag = document.createElement('script');
-                jqTag.type = 'text/javascript';
-                jqTag.src = 'https://code.jquery.com/jquery-3.5.1.js';
-                jqTag.onload = myJQueryCode;
-                headTag.appendChild(jqTag);
-            } else {
-                myJQueryCode();
+        if (start == 0) {
+            start = 1
+            //no boarders in continuous scroll mode
+            if (GM_config.get('webtoon')) {
+                document.querySelector("#amrapp > div.v-application--wrap > main > div > div > table").classList.add('webtoon');
             }
-        }
 
-        //detect space bar press and do scrollMagic from HakuNeko
-        function myJQueryCode() {
-            jQuery(document).on('keydown', function (e) {
-                if (e.keyCode == 32 && e.target == document.body) {
-                    e.preventDefault();
-                    scrollMagic();
+            //code probably only i need
+            if (GM_config.get('mangaTitle')) {
+                if (document.querySelector("head > title").textContent != 'Manga') {
+                    document.querySelector("head > title").textContent = 'Manga'
                 }
-            });
-        }
-
-        //code probably only i need
-        //this origonally just changed the title of the page to "manga" but t now fixes the starting half way issue
-        if (GM_config.get('mangaTitle') || GM_config.get('halfFix') || GM_config.get('mangaTitleName')) {
-            let observer = new MutationObserver(callback);
-            let options = {
-                characterData: true,
-                childList: true
+            } else if (GM_config.get('mangaTitleName')) {
+                if (!(document.querySelector("h4 > a") == null || document.querySelector("div.v-select__selection.v-select__selection--comma") == null)) {
+                    if (document.querySelector("head > title").textContent != document.querySelector("h4 > a").textContent + ' ' + document.querySelector("div.v-select__selection.v-select__selection--comma").textContent) {
+                        document.querySelector("head > title").textContent = document.querySelector("h4 > a").textContent + ' ' + document.querySelector("div.v-select__selection.v-select__selection--comma").textContent
+                    }
+                }
             }
-            observer.observe(document.querySelector("head > title"), options);
-        }
 
-        /*
-        in order
-        make the bookmark triange (top right of any scan) the correct size
-        make the scan 100% width (for portrait viewing)
-        dito
-        no boarders in side by side mode
-        */
-        if (GM_config.get('Triangle')) {
-            GM_addStyle(`
+            //if no jquiry then add jquiry
+            if (GM_config.get('magicScroll')) {
+                if (typeof jQuery == 'undefined') {
+                    var headTag = document.getElementsByTagName("head")[0];
+                    var jqTag = document.createElement('script');
+                    jqTag.type = 'text/javascript';
+                    jqTag.src = 'https://code.jquery.com/jquery-3.5.1.js';
+                    jqTag.onload = myJQueryCode;
+                    headTag.appendChild(jqTag);
+                } else {
+                    myJQueryCode();
+                }
+            }
+
+            //detect space bar press and do scrollMagic from HakuNeko
+            function myJQueryCode() {
+                jQuery(document).on('keydown', function (e) {
+                    if (e.keyCode == 32 && e.target == document.body) {
+                        e.preventDefault();
+                        scrollMagic();
+                    }
+                });
+            }
+
+            if (GM_config.get('mangaTitle') || GM_config.get('mangaTitleName')) {
+                let observer = new MutationObserver(callback);
+                let options = {
+                    characterData: true,
+                    childList: true
+                }
+                observer.observe(document.querySelector("head > title"), options);
+            }
+
+            /*
+            in order
+            make the bookmark triange (top right of any scan) the correct size
+            make the scan 100% width (for portrait viewing)
+            no boarders in side by side mode
+            */
+            if (GM_config.get('Triangle')) {
+                GM_addStyle(`
             .amr-triangle {
                 border-left: 0px solid transparent;
             }`)
-        }
-        if (GM_config.get('100Width')) {
-            GM_addStyle(`
+            }
+            if (GM_config.get('100Width')) {
+                GM_addStyle(`
             .scanContainer.res-w img {
                 width: 100%;
             }
@@ -180,18 +193,20 @@ function callbackk(mutations) {
             .amr-scan-container td {
                 padding: 0 !important;
             }`)
-        }
-        if (GM_config.get('webtoon')) {
-            GM_addStyle(`
+            }
+            if (GM_config.get('webtoon')) {
+                GM_addStyle(`
                 .amr-scan-container td {
                     padding: 0 !important;
                 }`)
+            }
         }
     }
 }
 
-//code probably only i need
-//this origonally just changed the title of the page to "manga" but t now fixes the starting half way issue
+function topAnim(){
+    jQuery('html,body').animate({ scrollTop: 0 }, 0);
+}
 
 function callback(mutations) {
     if (GM_config.get('mangaTitle')) {
@@ -204,11 +219,7 @@ function callback(mutations) {
             document.querySelector("head > title").textContent = title
         }
     }
-    if (GM_config.get('halfFix')) {
-        $('html,body').animate({ scrollTop: 0 }, 0);
-    }
 }
-
 
 let options = {
     characterData: true,
